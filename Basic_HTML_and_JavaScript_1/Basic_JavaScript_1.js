8282b6e7482fa64b94f1290a8d05b9eaf828c997
CristSwen <123016113+CristSwen@users.noreply.github.com>
2023-01-20 12:33:47 -0700

    Added paragraphs with color name and hex color
    Added two new paragraphs that both have different colors. One utilizing color name and the other hex code

--- Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -6,3 +6,9 @@ function My_First_Function() {
 	document.getElementById("Irish").innerHTML =
 	result;
 }
+<!DOCTYPE html>
+<html lang="eng">
+	<body>
+		<h1><font color="pink">Pink is my least favorite color.</font></h1>
+		<h2><font color+"#F0E68C">I didn't know Khaki was a color.</font></h2>
+	</body>
